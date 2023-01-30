de86edef7dac2cecb25958fca52d4a8229b50c53
TamaraKoval <kovalpsy@mail.ru>
2023-01-30 17:00:14 +0300

    Main test2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager test = new SalesManager(new int[]{50, 100, 60});
+        SalesManager test = new SalesManager(new int[]{50, 100, 60, 200, 30});
 
         int max = test.max();
         System.out.println("Проверяем работу метода класса: " + max);
